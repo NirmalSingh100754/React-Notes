b729c240ec07bbd6671c147163773f7d62b4a824
Nirmal Singh <nirmalsingh100754@hotmail.com>
2025-07-02 15:58:25 +0530

    fdytdhch

--- context-api-2/src/App.jsx
+++ context-api-2/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import ThemeBtn from "./components/ThemeButton.jsx";
+import Card from "./components/Card.jsx";
+import { ThemeProvider } from "./contexts/theme.js";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => {
+    setThemeMode("light");
+  }
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  useEffect(() => {
+  document.querySelector('html').classList.remove('dark','light');
+  document.querySelector('html').classList.add(themeMode);
+  }, [themeMode])
   return (
-    <>
-    <h1>Context-API</h1>
-    </>
-  )
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+        <ThemeBtn />
+        <div className="w-full max-w-sm mx-auto"></div>
+        <Card />
+      </div>
+    </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
